214c4d8891ee00a5776c134c2be1f149db1276da
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2022-12-24 19:58:02 +0100

    Object challenge started

--- Challenge #3 - Objects/script.js
+++ Challenge #3 - Objects/script.js
@@ -3,36 +3,56 @@ import {peeps, comments} from './data.js'
 // EASIER CHALLENGES (using the peeps array) //
 
 // 1. Count the number of people in the people array.
-const numberOfPeeps = peeps.length;
-console.log(numberOfPeeps);
-console.log('=================================');
+// const numberOfPeeps = peeps.length;
+// console.log('number of people:', numberOfPeeps);
+// console.log('=================================');
 
-// 2. List full names of people in a new array.
-const fullNamesArray = peeps.map(p => `${p.name.first} ${p.name.last}`);
-console.log(fullNamesArray);
-console.log('=================================');
+// // 2. List full names of people in a new array.
+// const fullNamesArray = peeps.map(p => `${p.name.first} ${p.name.last}`);
+// console.log('full names of people:', fullNamesArray);
+// console.log('=================================');
 
-// 3. Return “true” if everyone is older than 24.
-console.log(peeps.every(p => p.age > 24));
-console.log('=================================');
+// // 3. Return “true” if everyone is older than 24.
+// console.log('older than 24:', peeps.every(p => p.age > 24));
+// console.log('=================================');
 
-// 4. Return “true” if at least one person is younger than 26.
-console.log(peeps.some(p => p.age < 26));
-console.log('=================================');
+// // 4. Return “true” if at least one person is younger than 26.
+// console.log('at least one person is younger than 26:', peeps.some(p => p.age < 26));
+// console.log('=================================');
 
-// 5. Return a new array called “young peeps” of all peeps less than 30.
+// // 5. Return a new array called “young peeps” of all peeps less than 30.
+// const youngPeeps = peeps.filter(p => p.age < 30);
+// console.log('peeps less than 30:', youngPeeps);
+// console.log('=================================');
 
-// 6. Sort the peeps by age from oldest to youngest in an array called “sortedPeeps”.
+// // 6. Sort the peeps by age from oldest to youngest in an array called “sortedPeeps”.
+// const sortedPeeps = peeps.sort((a, b) => b.age - a.age);
+// console.log('Sorted Peeps', sortedPeeps);
+// console.log('=================================');
 
-// 7. Create a new array called “firstNamePeeps” with only the first names of the peeps.
+// // 7. Create a new array called “firstNamePeeps” with only the first names of the peeps.
+// const firstNamePeeps = peeps.map(P => P.name.first);
+// console.log('first names of the peeps', firstNamePeeps);
+// console.log('=================================');
 
 // HARDER CHALLENGES (using the comments object and peeps array) //
 
 // 1. List all the comments in an array of strings called “commentsArray”
+const commentsArray = Object.values(comments).map(com => com.text);
+console.log('List all the comments', commentsArray);
+console.log('======================================================');
 
 // 2. Return all comments with the word “love” in the comment in a new array called “loveComments”
+const loveComments = Object.values(comments).filter(com => com.text.includes('love'));
+console.log('comments with the word “love”', loveComments);
+console.log('======================================================');
 
 // 3. List all the comments in an array of strings called “sortedCommentsArray" from lowest rating to highest rating. Ignore comments without a rating.
+const sortedCommentsArray = Object.values(comments)
+    .filter(com => com.rating)
+    .sort((a, b) => a.rating - b.rating);
+console.log('Comments from lowest rating to highest ', sortedCommentsArray);
+console.log('======================================================');
 
 // 4. Return a new object called “commentObj” with the comment id as a key and the comment text as the value.
 
