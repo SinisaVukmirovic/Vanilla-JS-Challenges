5a331fd839b2a46886f4ade7e7e7160573282755
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2023-01-02 00:28:51 +0100

    4 fetch challenges

--- Challenge #4 - Fetch API/main.js
+++ Challenge #4 - Fetch API/main.js
@@ -14,9 +14,58 @@ const BASEURL = "https://dummyjson.com";
 // }
 
 // 2. Update the "customFetch" function to take in a endpoint and handle errors (including a wrong endpoint)
-const customFetch = async (endpoint) => {
+
+// const customFetch = async (endpoint) => {
+//     try {
+//         const results = await fetch(`${BASEURL}/${endpoint}`);
+//         // console.log(results)
+//         if (!results.ok) {
+//             throw new Error(`${results.status}: ${results.statusText}`);
+//         }
+//         const data = await results.json();
+    
+//         console.log(data);
+//         return data;
+//     } catch(err) {
+//         console.error(err.message);
+//     }
+// }
+
+// customFetch('sdfsdfsdf');
+// customFetch('products');
+
+// 3. Create a function called "getProducts" that returns all the products in an array. Then list all the products in the DOM in an unordered list.
+
+// const getProducts = async () => {
+//     const results = await fetch(`${BASEURL}/products`);
+//     const data = await results.json();
+//     // console.log(data.products);
+//     return data.products;
+// }
+
+// const addUl = async () => {
+//     const ul = document.createElement('ul');
+//     const products = await getProducts();
+//     // console.log(products);
+//     products.map(prod => {
+//         const li = document.createElement('li');
+//         li.innerText = prod.title;
+//         ul.append(li);
+//     });
+//     document.querySelector('body').append(ul);
+// }
+
+// addUl();
+
+
+// 4. Update the "customFetch" function to handle a delete, add, or update a request.
+/*
+- Use the updated function to update and then delete a product by it’s ID. (FYI, "https://dummyjson.com/products/1" will access the product with the id of 1)
+- Use the updated function to POST a new product to the list of products. (FYI, "https://dummyjson.com/products/add" will allow POST requests)
+*/
+const customFetch = async (endpoint, options) => {
     try {
-        const results = await fetch(`${BASEURL}/${endpoint}`);
+        const results = await fetch(`${BASEURL}/${endpoint}`, {...options});
         // console.log(results)
         if (!results.ok) {
             throw new Error(`${results.status}: ${results.statusText}`);
@@ -30,17 +79,21 @@ const customFetch = async (endpoint) => {
     }
 }
 
-customFetch('sdfsdfsdf');
-customFetch('products');
+// customFetch('products/1', {
+//     method: 'DELETE'
+// });
 
-// 3. Create a function called "getProducts" that returns all the products in an array. Then list all the products in the DOM in an unordered list.
+// customFetch('products/1', {
+//     method: 'PUT',
+//     body: JSON.stringify({'title': 'UPDATED!'}),
+//     headers: { 'Content-type': 'application/json' }
+// });
 
-// 4. Update the "customFetch" function to handle a delete, add, or update a request.
-/*
-- Use the updated function to update and then delete a product by it’s ID. (FYI, "https://dummyjson.com/products/1" will access the product with the id of 1)
-- Use the updated function to POST a new product to the list of products. (FYI, "https://dummyjson.com/products/add" will allow POST requests)
-*/
-
+customFetch('products/add', {
+    method: 'POST',
+    body: JSON.stringify({'title': 'NEW ITEM!'}),
+    headers: { 'Content-type': 'application/json' }
+});
 
 // use the /users, /posts/, and /comments endpoints for #5–7
 // 5. Create a function called "getFilteredUsers" that console logs and returns all the users who are 21 years old in a new array of objects in the following structure:
